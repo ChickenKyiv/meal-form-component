873e53df387062dc20d8e3d6d8910772224e727f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-04 10:38:45 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ExampleView from '@v/ExampleView'
+
+
+export {
+  ExampleView
+
+}
---
+++ src/views/ExampleView.js
@@ -0,0 +1,65 @@
+import React, { Component, Fragment } from 'react';
+import _ from "lodash";
+
+// import {
+//   HeaderShowcase,
+//   LoadMoreList
+// } from '@groceristar/antd-showcase-components';
+//
+// import {
+//   Card1,
+//   Card2,
+//   Card3,
+//   Card4,
+//   Card5,
+//
+//   ClearCardsExample,
+//   DefaultCardsExample
+// } from '@groceristar/cards-wrapper'
+
+
+
+// import RecipeListExample from './components/RecipeList/RecipeList'
+
+
+
+
+// import RecipeListExample from './components/RecipeList/RecipeList'
+
+
+import "antd/dist/antd.css";
+// import "./../styles.css";
+
+// import {
+//   getFirstFiveRecipes
+// } from "../selector";
+
+
+// const list = getFirstFiveRecipes()
+
+class ExampleCardsView extends Component {
+
+    state = {
+      // data: getGroceryCollection(), //Did we need it here? i think we didn use all data from this method
+
+    };
+
+  render() {
+
+
+    const list = this.props.list;
+
+    return (
+      <Fragment>
+        <div className="main">
+
+
+
+        </div>
+      </Fragment>
+
+    );
+  }
+}
+
+export default ExampleCardsView;
